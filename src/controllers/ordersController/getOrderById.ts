e9f2ac33a2bd30664748f4a976e21b4ca6a0ba26
Md Sakibul Hasan <sakibal623@gmail.com>
2024-08-21 22:25:45 +0600

    feat: get order by _id.

--- src/controllers/ordersController/getOrderById.ts
+++ src/controllers/ordersController/getOrderById.ts
@@ -1,5 +1,17 @@
 import {Request, Response} from "express";
+import {generateResponse} from "../../utils/generateResponse";
+const Order = require('../../models/Order')
 
 export default async (req: Request, res: Response) => {
+    const {id} = req.params;
 
+    try {
+        const order = await Order.find({_id: id});
+
+        res.status(200).json(order);
+    } catch (err: any) {
+        res.status(500).json(
+            generateResponse(true, 500, 'Error', err.message)
+        );
+    }
 }
